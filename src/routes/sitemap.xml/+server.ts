5bcbe498a1e2487adc6c8cb64a8cf308feca2a97
bmcool <bloggerbm@gmail.com>
2025-12-04 22:06:16 +0800

    no message

--- src/routes/sitemap.xml/+server.ts
+++ src/routes/sitemap.xml/+server.ts
@@ -19,8 +19,7 @@ export const GET: RequestHandler = async () => {
 	const allTags = new Set([...tagsZh, ...tagsEn]);
 	
 	const sitemap = `<?xml version="1.0" encoding="utf-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
-        xmlns:xhtml="http://www.w3.org/1999/xhtml">
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 	<!-- Homepage -->
 	<url>
 		<loc>${SITE_URL}/</loc>
